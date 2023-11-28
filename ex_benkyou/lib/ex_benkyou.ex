9988670e6b063c3843b9c795de78d7d04414ad4b
WJ Lee <70941958+wjlee2020@users.noreply.github.com>
2023-11-29 08:40:32 +0900

    init elixir benkyou

---
+++ ex_benkyou/lib/ex_benkyou.ex
@@ -0,0 +1,18 @@
+defmodule ExBenkyou do
+  @moduledoc """
+  Documentation for `ExBenkyou`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBenkyou.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ex_benkyou/test/ex_benkyou_test.exs
@@ -0,0 +1,8 @@
+defmodule ExBenkyouTest do
+  use ExUnit.Case
+  doctest ExBenkyou
+
+  test "greets the world" do
+    assert ExBenkyou.hello() == :world
+  end
+end
